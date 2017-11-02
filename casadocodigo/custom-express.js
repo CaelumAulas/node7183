d17b5c7ec7286c7946f1df8c558594d1856246b1
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-02 20:19:20 -0200

    5.5 Exercícios: Suportando JSON como formato para cadastro

--- casadocodigo/custom-express.js
+++ casadocodigo/custom-express.js
@@ -7,6 +7,7 @@ module.exports = function() {
   app.set('view engine', 'ejs')
 
   app.use(bodyParser.urlencoded({extended: true}))
+  app.use(bodyParser.json())
   app.use(express.static('./public'))
 
   load('infra')
